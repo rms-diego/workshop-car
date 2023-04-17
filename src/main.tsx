7c6290e432c8473ccc9b37f4241a6ea1bd053d65
Diego Ramos <diegorbrito9@gmail.com>
2023-04-17 11:48:40 -0300

    refactor: adicionando alias de importação nas rotas no arquivo 'main'

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import { BrowserRouter } from "react-router-dom";
 
-import { AppRoutes } from "./Routes";
+import { AppRoutes } from "@/Routes";
 
 import "./globalStyles.scss";
 
