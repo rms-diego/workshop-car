6794266be08d497fbd5f7e65122fb76bbff84e9b
Diego Ramos <diegorbrito9@gmail.com>
2023-04-17 11:45:03 -0300

    feat: criando alias para importação no projeto

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import path from "path";
+
+import react from "@vitejs/plugin-react";
+
+console.log(path.resolve(__dirname, "./src/"));
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src/"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@layout": path.resolve(__dirname, "./src/Layout"),
+    },
+  },
+});
