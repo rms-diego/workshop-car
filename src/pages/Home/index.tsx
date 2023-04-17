ef61399f14e8305cfb48e1a448e8cbd1a85d0f4a
Diego Ramos <diegorbrito9@gmail.com>
2023-04-17 12:07:20 -0300

    feat: criando a pagina home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+
+import styles from "./styles.module.scss";
+
+import Layout from "@/Layout";
+import logo from "@/assets/logo.png";
+
+export function Home() {
+  return (
+    <Layout>
+      <>
+        <img src={logo} alt="" className={styles.logo} />
+        <Link to="/customer/generate-qrcode" className={styles.linkContainer}>
+          <button>Cadastrar um cliente</button>
+        </Link>
+
+        <Link to="*" className={styles.linkContainer}>
+          <button>Ler qr code do cliente</button>
+        </Link>
+
+        <Link to="*" className={styles.linkContainer}>
+          <button>Todos serviços criados</button>
+        </Link>
+      </>
+    </Layout>
+  );
+}
