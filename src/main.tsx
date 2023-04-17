48dff5313cc0bdfaad99970cd41feb9e388d0001
Diego Ramos <diegorbrito9@gmail.com>
2023-04-17 17:18:12 -0300

    refactor: retirando o 'BrowseRouter' do arquivo 'main'

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { BrowserRouter } from "react-router-dom";
-
 import { AppRoutes } from "@/Routes";
 
 import "./globalStyles.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <AppRoutes />
   </React.StrictMode>
 );
