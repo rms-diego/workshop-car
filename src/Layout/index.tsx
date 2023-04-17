24f1c4b2092ab4d817d79d5825557168c433f592
Diego Ramos <diegorbrito9@gmail.com>
2023-04-17 13:30:46 -0300

    new: adicionando botão para voltar a pagina

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -1,11 +1,35 @@
 import { PropsWithChildren } from "react";
+import { FaArrowLeft } from "react-icons/fa";
 
 import styles from "./styles.module.scss";
 
+import { useLocation, useNavigate } from "react-router-dom";
+
 export default function Layout({ children }: PropsWithChildren) {
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+
+  const goBackPage = () => {
+    switch (pathname) {
+      case "/customer/generate-qrcode": {
+        return navigate("/");
+      }
+    }
+  };
+
   return (
     <div className={styles.container}>
-      <main>{children}</main>
+      <main>
+        {pathname !== "/" && (
+          <FaArrowLeft
+            className={styles.arrowLeft}
+            size={30}
+            onClick={() => goBackPage()}
+          />
+        )}
+
+        {children}
+      </main>
     </div>
   );
 }
