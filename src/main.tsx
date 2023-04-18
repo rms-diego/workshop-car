a8faeefeaf1d47fbc980b961ca014435d271458c
Diego Ramos <diegorbrito9@gmail.com>
2023-04-17 22:25:59 -0300

    refactor: criando pasta 'styles' para colocar arquivos de estilo globais

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import { AppRoutes } from "@/Routes";
 
-import "./globalStyles.scss";
+import "./styles/globalStyles.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
