e7cbd618135d3b7e49f1ff40b1047837dc4732f9
Diego Ramos <diegorbrito9@gmail.com>
2023-04-17 22:28:28 -0300

    feat: criando rota para a pagina de criar cliente e de exibir qrcode

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,11 +1,18 @@
-import { Route, Routes, Navigate } from "react-router-dom";
-import { Home } from "../pages/Home";
+import { Route, Routes, Navigate, BrowserRouter } from "react-router-dom";
+
+import { Customer } from "@/pages/Customer";
+import { Home } from "@/pages/Home";
+import { QrCode } from "@/pages/QrCode";
 
 export function AppRoutes() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/customer/generate-qrcode" element={<Customer />} />
+        <Route path="/qr-code/:customerId" element={<QrCode />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
